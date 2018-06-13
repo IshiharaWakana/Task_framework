8f8fdaec9e4e9ba14c2021a5ccb3abccbc84b76d
admin <admin@admin-HP>
2018-06-13 12:32:13 +0900

    test

--- src/main/java/jp/co/axiz/web/ServletInitializer.java
+++ src/main/java/jp/co/axiz/web/ServletInitializer.java
@@ -1,4 +1,4 @@
-package jp.co.example;
+package jp.co.axiz.web;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/jp/co/axiz/web/TaskFrameworkApplication.java
+++ src/main/java/jp/co/axiz/web/TaskFrameworkApplication.java
@@ -1,4 +1,4 @@
-package jp.co.example;
+package jp.co.axiz.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
